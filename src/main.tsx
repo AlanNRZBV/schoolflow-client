3e49677156b18f16d2788b70b2f67e8a0562b5fa
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2026-03-20 19:14:02 +0600

    feat: add axios instance and react-query setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import './index.css';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from '@/routeTree.gen.ts';
 import { Container, CssBaseline } from '@mui/material';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/api/queryClient.ts';
 
 const router = createRouter({ routeTree });
 
@@ -19,9 +21,11 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <CssBaseline />
-      <Container maxWidth="sm">
-        <RouterProvider router={router} />
-      </Container>
+      <QueryClientProvider client={queryClient}>
+        <Container maxWidth="sm">
+          <RouterProvider router={router} />
+        </Container>
+      </QueryClientProvider>
     </StrictMode>
   );
 }
