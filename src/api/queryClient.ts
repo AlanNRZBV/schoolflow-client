a64eed8cc4e2c36226aaf4b5ee22d97f5471e7ff
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2026-03-24 18:51:50 +0600

    feat: customize query retry logic to handle connection errors

--- src/api/queryClient.ts
+++ src/api/queryClient.ts
@@ -3,10 +3,15 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      retry: (failureCount, error) => {
+        if (error.message.includes('ERR_CONNECTION_REFUSED')) {
+          return false;
+        }
+        return failureCount < 2;
+      },
       staleTime: 5 * 60 * 1000,
       gcTime: 10 * 60 * 1000,
       refetchOnWindowFocus: false,
-      retry: 1,
       retryOnMount: true,
     },
 
