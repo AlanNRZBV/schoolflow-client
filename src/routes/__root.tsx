7409b79e5f4aff67066b7f51ba837ba6ebc30b40
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2026-03-27 16:19:42 +0600

    feat: integrate TanStack devtools, React Form, and Zod; update GlobalLoader import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,9 @@
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import type { QueryClient } from '@tanstack/react-query';
-import GlobalLoader from '@/components/GlobalLoader/GlobalLoader.tsx';
+import { TanStackDevtools } from '@tanstack/react-devtools';
+import { formDevtoolsPlugin } from '@tanstack/react-form-devtools';
+import { GlobalLoader } from '@/components/Global';
 import { Paper } from '@mui/material';
 
 interface MyRouterContext {
@@ -12,14 +14,14 @@ const RootLayout = () => (
   <Paper sx={{ backgroundColor: 'background.default' }}>
     <Outlet />
     <TanStackRouterDevtools />
-  </>
+    <TanStackDevtools plugins={[formDevtoolsPlugin()]} />
+  </Paper>
 );
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: RootLayout,
   pendingComponent: GlobalLoader,
   errorComponent: ({ error }) => {
-    console.error('Root route error:', error);
     return (
       <div className="p-8 text-red-600">
         <h2>Auth check failed</h2>
